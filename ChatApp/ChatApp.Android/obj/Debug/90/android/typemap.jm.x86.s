55873b83353a473830c6891e0e10f0a2ff223f90
Blissful18 <nicole_mhelly@yahoo.com>
2021-05-12 11:46:30 +0800

    Initial commit

---
+++ ChatApp/ChatApp.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 9534695C28882C8E */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	3032
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 794385
+	.include	"typemap.jm.inc"
---
+++ ChatApp/ChatApp/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatApp
+{
+    public partial class App : Application
+    {
+        public static float screenWidth { get; set; }
+        public static float screenHeight { get; set; }
+        public static float appScale { get; set; }
+
+
+        public App()
+        {
+            InitializeComponent();
+
+            bool isLoggedIn = Current.Properties.ContainsKey("IsLoggedIn") ? Convert.ToBoolean(Current.Properties["IsLoggedIn"]) : false;
+            if (isLoggedIn == false)
+            {
+                MainPage = new NavigationPage(new MainPage());
+            }
+            else
+            {
+                MainPage = new NavigationPage(new TabbedPage1());
+            }
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
